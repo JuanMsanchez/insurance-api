4e70031b8a9f2ef736039831cfaea3967d904d04
Juan Manuel Sanchez <juanmanuel.sanchez@rga.com>
2019-10-23 23:50:54 -0300

    added /clients and /policies routes

--- modules/insurance/routes.js
+++ modules/insurance/routes.js
@@ -2,16 +2,19 @@ const Router = require('koa-router');
 
 module.exports = ({ insuranceService }) => {
     const router = new Router();
+    
+    const strValidation = new RegExp(/^[A-Z a-z 0-9-_]*$/);
+    const emailValidation = new RegExp(/\S+@\S+\.\S+/);
 
-    router.get('/insurance/token', async (ctx, next) => {
+    router.get('/insurance/token', async (ctx) => {
         const { email } = ctx.query;
-        let token;
 
-        if (!/\S+@\S+\.\S+/.test(email))
+        if (!emailValidation.test(email))
             return ctx.badRequest({ message: 'Invalid or missing email param' });
 
         try {
-            token = await insuranceService.getAccessToken(email);
+            const token = await insuranceService.getAccessToken(email);
+            return ctx.ok({ token });
         } catch (e) {
             if(e.message === 'Invalid user')
                 return ctx.badRequest({ message: 'Invalid user' });
@@ -19,8 +22,42 @@ module.exports = ({ insuranceService }) => {
             console.log(e);
             return ctx.internalServerError();
         }
+    });
+    
+    router.get('/insurance/clients', async (ctx) => {
+        const { policyId, name, id } = ctx.query;
 
-        return ctx.ok({ token });
+        if (!(
+            strValidation.test(policyId) && 
+            strValidation.test(name) && 
+            strValidation.test(id))
+        ) {
+            return ctx.badRequest({ message: 'Ilegal values on filters' });
+        }
+
+        try {
+            const clients = await insuranceService.getClients({ policyId, name, id });
+            return ctx.ok({ clients });
+        } catch (e) {
+            console.log(e);
+            return ctx.internalServerError();
+        }
+    });
+    
+    router.get('/insurance/policies', async (ctx) => {
+        const { username } = ctx.query;
+
+        if (!strValidation.test(username)) {
+            return ctx.badRequest({ message: 'Ilegal values on filters' });
+        }
+
+        try {
+            const policies = await insuranceService.getPolicies({ username });
+            return ctx.ok({ policies });
+        } catch (e) {
+            console.log(e);
+            return ctx.internalServerError();
+        }
     });
     
     return router.routes();
--- modules/insurance/routes.test.js
+++ modules/insurance/routes.test.js
@@ -12,6 +12,7 @@ describe('insurance routes test', () => {
     const CLIENT_ID = 'e8fd159b-57c4-4d36-9bd7-a59ca13057bb';
     const POLICY_ID = 'be4bf877-5a72-4ae2-b8f5-3c79e21fc829';
     const USER_EMAIL = 'manningblankenship@quotezart.com';
+    const USER_NAME = 'Manning';
 
     const mockedPolicies = nock(host)
     .get(policiesPath)
@@ -38,7 +39,7 @@ describe('insurance routes test', () => {
     .reply(200, {
         clients: [{
             "id": CLIENT_ID,
-            "name": "Manning",
+            "name": USER_NAME,
             "email": USER_EMAIL,
             "role": "admin"
         }],
@@ -60,5 +61,52 @@ describe('insurance routes test', () => {
         const response = await agent.get(`/insurance/token?email=fake@mail.com`)
         expect(response.status).toEqual(400);
     });
+
+    test('Test /insurance/clients route with no params', async () => {
+        const response = await agent.get(`/insurance/clients`)
+        expect(response.status).toEqual(200);
+    });
+
+    test('Test /insurance/clients route with valid params', async () => {
+        const policyFiltered = await agent.get(`/insurance/clients?policyId=${POLICY_ID}`)
+        expect(policyFiltered.status).toEqual(200);
+        assert(policyFiltered.body.clients[0].id === CLIENT_ID, 'Error filtering by policy');
+
+        const nameFiltered = await agent.get(`/insurance/clients?name=${USER_NAME}`)
+        expect(nameFiltered.status).toEqual(200);
+        assert(nameFiltered.body.clients[0].id === CLIENT_ID, 'Error filtering by name');
+
+        const idFiltered = await agent.get(`/insurance/clients?id=${CLIENT_ID}`)
+        expect(idFiltered.status).toEqual(200);
+        assert(idFiltered.body.clients[0].id === CLIENT_ID, 'Error filtering by id');
+    });
+
+    test('Test /insurance/clients route with multiple valid params', async () => {
+        const query = `policyId=${POLICY_ID}&id=${CLIENT_ID}&name=${USER_NAME}`;
+        const response = await agent.get(`/insurance/clients?${query}`)
+        expect(response.status).toEqual(200);
+        assert(response.body.clients[0].id === CLIENT_ID,
+            'Error filtering by multiple params');
+    });
   
+    test('Test /insurance/clients route with ilegal values params', async () => {
+        const response = await agent.get(`/insurance/clients?name=$()`)
+        expect(response.status).toEqual(400);
+    });
+
+    test('Test /insurance/policies route with no params', async () => {
+        const response = await agent.get(`/insurance/policies`)
+        expect(response.status).toEqual(200);
+    });
+
+    test('Test /insurance/policies route with valid params', async () => {
+        const response = await agent.get(`/insurance/policies?username=${USER_NAME}`)
+        expect(response.status).toEqual(200);
+        assert(response.body.policies[0].id === POLICY_ID, 'Error filtering by username');
+    });
+
+    test('Test /insurance/policies route with ilegal values params', async () => {
+        const response = await agent.get(`/insurance/policies?username=$()`)
+        expect(response.status).toEqual(400);
+    });
 });
